90adcdffb79900df313bd6f4d587437c134b948a
OliverLadislavTomka <xtomkao@stuba.sk>
2021-04-25 11:45:10 +0200

    init

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// TODO: miesto pre pripojenie externej funkcionality z ostatnych
+//  suborov projektu.
+
+// TODO: miesto pre implementaciu vasich vlastnych funkcii, datovych
+//  typov a vlastne makra.
+
+int main(int argc, char *argv[]) {
+
+    // TODO: miesto pre implementaciu zadania.
+
+    return 0;
+}
